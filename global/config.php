b6e6204f662785db597e7ed5838c87e0adc4cdf6
Manlio Emiliano Terán Ramos <manlioelnum1@hotmail.com>
2019-09-09 15:40:06 -0500

    test accounts

--- global/config.php
+++ global/config.php
@@ -1,11 +1,18 @@
 <?php
 
+/*
+
+luisdcm10-facilitator@gmail.com
+luisdcm10-buyer@gmail.com
+Password: Crisostomo_07.!*
+*/
+
     define("KEY","develoteca");
     define("COD","AES-128-ECB");
 
     define("SERVIDOR","localhost");
     define("USUARIO","root");
-    define("PASSWORD","root");
+    define("PASSWORD","");
     define("BD","tienda")   
 
 ?>
